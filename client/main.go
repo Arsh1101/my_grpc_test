de2a70b9b78e7ac2dbfa07b3e9959dfddf82d893
arsh <arshia.alidosti.72@gmail.com>
2023-04-11 01:07:56 -0700

    Fixrd dir's.

---
+++ client/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "example.com/go-grpc/filetransfer"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewFileTransferClient(conn)
+
+	// Open the file to send.
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatalf("could not open file: %v", err)
+	}
+	defer file.Close()
+
+	// Create a stream to send the file chunks.
+	stream, err := c.SendFile(context.Background())
+	if err != nil {
+		log.Fatalf("could not send file: %v", err)
+	}
+
+	// Send the file chunks to the server.
+	chunkSize := 1024 // 1KB
+	buf := make([]byte, chunkSize)
+	for {
+		n, err := file.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("could not read file: %v", err)
+		}
+		if err := stream.Send(&pb.FileChunk{Data: buf[:n]}); err != nil {
+			log.Fatalf("could not send chunk: %v", err)
+		}
+	}
+
+	// Close the stream and wait for the response from the server.
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("could not receive response: %v", err)
+	}
+	log.Printf("Response: %v", resp)
+}
